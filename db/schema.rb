bf6b8eb46ec65b017ec6502e002a816bd2d413b4
Lucas Virgil <virgill@email.unc.edu>
2022-04-13 19:20:44 -0400

    rollback migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_13_203907) do
+ActiveRecord::Schema.define(version: 2022_04_12_195240) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "description"
-    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
